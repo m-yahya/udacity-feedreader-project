4d09e0b7339547be134247e0180000865d582f54
Muhammad Yahya <yahya_gis@hotmail.com>
2019-01-29 17:01:50 +0100

    Initial Entries test suite completed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -72,18 +72,25 @@ $(function() {
   });
 
   /* TODO: Write a new test suite named "Initial Entries" */
+  describe('Initial Entries', () => {
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+    beforeEach(done => {
+      loadFeed(0, done);
+    });
+    it('has entry element(s)', () => {
+      expect($('.feed .entry').length).not.toBe(0);
+    });
 
-  /* TODO: Write a test that ensures when the loadFeed
-   * function is called and completes its work, there is at least
-   * a single .entry element within the .feed container.
-   * Remember, loadFeed() is asynchronous so this test will require
-   * the use of Jasmine's beforeEach and asynchronous done() function.
-   */
+    /* TODO: Write a new test suite named "New Feed Selection" */
 
-  /* TODO: Write a new test suite named "New Feed Selection" */
-
-  /* TODO: Write a test that ensures when a new feed is loaded
-   * by the loadFeed function that the content actually changes.
-   * Remember, loadFeed() is asynchronous.
-   */
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
+  });
 }());
